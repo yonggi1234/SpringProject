8548075ece616ce3b423104d28e764051f34296c
DESKTOP-N5KD4EV\litte <littedeer108@gmail.com>
2025-04-14 17:49:55 +0900

    MVC 실습

--- src/main/java/project/projectspring/controller/HelloController.java
+++ src/main/java/project/projectspring/controller/HelloController.java
@@ -4,6 +4,7 @@ package project.projectspring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -12,5 +13,12 @@ public class HelloController {
         model.addAttribute("data", "Hello World");
 
         return "hello";
+    }
+
+    @GetMapping("first-mvc")
+    public String firstMvc(@RequestParam(value = "name", required = false) String name, Model model){
+        model.addAttribute("name", name);
+        return "abc";
+
     }
 }
