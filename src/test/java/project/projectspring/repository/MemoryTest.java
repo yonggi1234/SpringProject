fa639131889a6822ef3472748fab9d48e9943b69
DESKTOP-N5KD4EV\litte <littedeer108@gmail.com>
2025-04-18 16:41:57 +0900

    코드 정리

--- src/test/java/project/projectspring/repository/MemoryTest.java
+++ src/test/java/project/projectspring/repository/MemoryTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import project.projectspring.domain.Member;
 
 import java.util.List;
-import java.util.Optional;
 
 public class MemoryTest {
     MemoryMemberRepository memberRepository = new MemoryMemberRepository();
